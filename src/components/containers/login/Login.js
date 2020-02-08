ecaf491f4ae34c20e9110240f68ee2b90cc8d97b
thaydds <thayronedds@gmail.com>
2020-02-08 12:32:13 -0300

    config react-router and init login page

---
+++ src/components/containers/login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Astron-Edu</h2>
+      <h3>Login</h3>
+      <input />
+      <input />
+    </div>
+  );
+};
+
+export default Login;
